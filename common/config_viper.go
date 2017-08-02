fba860ca7ec92bd25c9c3250130e2a5e8218e51c
Zignd <zignd.igor@gmail.com>
2017-08-02 20:35:32 -0300

    Fix flags configurations

--- common/config_viper.go
+++ common/config_viper.go
@@ -11,13 +11,13 @@ import (
 
 // ConfigFlags configures the application flags.
 func ConfigFlags() error {
-	pflag.String("config-file", "$XDG_CONFIG_HOME/config.json", "configuration file with information regarding the connection to the server, expressions and actions to be taken")
+	pflag.String("config-file", "$XDG_CONFIG_HOME/config.json", "Configuration file with information regarding the connection to the server, expressions and actions to be taken.")
+	pflag.Bool("run-once", false, "Performs the verifications defined in the configuration file only once. When set to `true` the `Frequency` setting is ignored.")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
 
 	configFile := viper.GetString("config-file")
 	if configFile == "$XDG_CONFIG_HOME/config.json" {
-		println(1)
 		viper.SetConfigName("config")
 		viper.SetConfigType("json")
 		if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
@@ -26,11 +26,13 @@ func ConfigFlags() error {
 			viper.AddConfigPath("$HOME/.config/lophutch")
 		}
 	} else {
-		if fi, err := os.Stat(configFile); fi.IsDir() || os.IsNotExist(err) {
-			return errors.Wrap(errors.Wrapf(err, "could not find file %s", configFile), "test")
+		if fi, err := os.Stat(configFile); (fi != nil && fi.IsDir()) || os.IsNotExist(err) {
+			return errors.Wrapf(err, "could not find file %s", configFile)
 		}
 		viper.SetConfigFile(configFile)
 	}
+
+	viper.ReadInConfig()
 
 	return nil
 }
