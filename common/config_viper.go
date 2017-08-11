ff7a9fbef932c488a4672f41fc940d0706b51a38
zignd <zignd.igor@gmail.com>
2017-08-11 14:46:56 -0300

    Fix viper configuration

--- common/config_viper.go
+++ common/config_viper.go
@@ -11,15 +11,15 @@ import (
 
 // ConfigFlags configures the application flags.
 func ConfigFlags() error {
-	pflag.String("config-file", "$XDG_CONFIG_HOME/config.json", "Configuration file with information regarding the connection to the server, expressions and actions to be taken.")
+	pflag.String("config-file", "$XDG_CONFIG_HOME/config.yaml", "Configuration file with information regarding the connection to the server, expressions and actions to be taken.")
 	pflag.Bool("run-once", false, "Performs the verifications defined in the configuration file only once. When set to `true` the `Frequency` setting is ignored.")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
 
 	configFile := viper.GetString("config-file")
-	if configFile == "$XDG_CONFIG_HOME/config.json" {
+	if configFile == "$XDG_CONFIG_HOME/config.yaml" {
 		viper.SetConfigName("config")
-		viper.SetConfigType("json")
+		viper.SetConfigType("yaml")
 		if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
 			viper.AddConfigPath(path.Join(xdgConfigHome, "lophutch"))
 		} else {
